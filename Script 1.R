2d33ed4b204dcbd48b70624baef0c6556e43cf77
Sebastian Cano Arias <159856881+Thesleshart@users.noreply.github.com>
2025-02-28 12:32:46 -0500

    Primera clase de probabilidad implementado R

---
+++ Script 1.R
@@ -0,0 +1,18 @@
+#practica 1, R para la probabilidad
+#decimal es = .
+#separadores son = ,
+
+#operaciones
+2+4
+2-4
+2*4
+2/4
+raiz = sqrt(5)
+round(raiz,3) #operacion que nos dara decimales, a cuantos decimales quiero 
+2^4 # elebado 
+
+x <- c(2,4,6,8)
+y <- c(1,5,7,9)
+
+plot(x,y)
+
